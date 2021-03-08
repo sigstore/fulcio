08ec07b8ebeb8aa22e2dfca311bc9b657415f202
Dan Lorenc <dlorenc@google.com>
2021-03-07 19:07:06 -0600

    Add some more unit tests and refactor to make them easier.
    Signed-off-by: Dan Lorenc <dlorenc@google.com>

--- pkg/ca/ca.go
+++ pkg/ca/ca.go
@@ -1,3 +1,19 @@
+/*
+Copyright © 2021 Dan Lorenc <lorenc.d@gmail.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package ca
 
 import (
@@ -43,6 +59,7 @@ func Check(pub []byte, proof string, email string) bool {
 
 func Req(email string, pemBytes []byte) *privatecapb.CreateCertificateRequest {
 	parent := viper.GetString("gcp_private_ca_parent")
+	// TODO, use the right fields :)
 	return &privatecapb.CreateCertificateRequest{
 		Parent: parent,
 		Certificate: &privatecapb.Certificate{
