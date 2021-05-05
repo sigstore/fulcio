d1196c73364d2f9fa26a2d4c4f79b189e6f7513b
Asra Ali <asraa@google.com>
2021-05-05 10:35:37 -0400

    Add timestamp cert issue.
    Co-authored-by: Priya Wadhwa <priyawadhwa@google.com>
    Signed-off-by: Asra Ali <asraa@google.com>

---
+++ cmd/client/app/generate.go
@@ -0,0 +1,155 @@
+/*
+Copyright © 2021 Dan Lorenc <lorenc.d@gmail.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package app
+
+import (
+	"context"
+	"encoding/asn1"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/sigstore/fulcio/pkg/log"
+
+	"github.com/pkg/errors"
+	"github.com/sigstore/fulcio/pkg/ca"
+	"github.com/sigstore/fulcio/pkg/ctl"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	privateca "google.golang.org/genproto/googleapis/cloud/security/privateca/v1beta1"
+	privatecapb "google.golang.org/genproto/googleapis/cloud/security/privateca/v1beta1"
+	"google.golang.org/protobuf/types/known/durationpb"
+)
+
+var (
+	pubKeyFile string
+	uri        string
+)
+
+var generateCmd = &cobra.Command{
+	Use:   "generate",
+	Short: "Generate a fulcio-signed certificate",
+	Long:  `Generate is used to create a fulcio-signed certificate without OIDC`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := generateCert(context.Background()); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	},
+}
+
+func generateCert(ctx context.Context) error {
+	pemBytes, err := ioutil.ReadFile(filepath.Clean(pubKeyFile))
+	if err != nil {
+		return errors.Wrap(err, "reading pub key file")
+	}
+	parent := viper.GetString("gcp-private-ca-parent")
+	timestamping := viper.GetBool("timestamping")
+	req := cert(parent, uri, pemBytes, timestamping)
+	resp, err := ca.Client().CreateCertificate(ctx, req)
+	if err != nil {
+		return errors.Wrap(err, "creating cert")
+	}
+
+	// Submit to CTL
+	log.Logger.Info("Submitting cert to CT log")
+	ctURL := viper.GetString("ct-log-url")
+	c := ctl.New(ctURL)
+	ct, err := c.AddChain(resp.PemCertificate, resp.PemCertificateChain)
+	if err != nil {
+		return errors.Wrap(err, "add chain")
+	}
+	log.Logger.Info("CTL Submission Signature Received: ", ct.Signature)
+	log.Logger.Info("CTL Submission ID Received: ", ct.ID)
+
+	// print cert and cert chain to STDOUT
+	fmt.Println(resp.GetPemCertificate())
+	fmt.Println("")
+	fmt.Println(resp.GetPemCertificateChain())
+	return nil
+}
+
+func cert(parent, uri string, pemBytes []byte, timestamping bool) *privatecapb.CreateCertificateRequest {
+	baseKeyUsage := privatecapb.KeyUsage_KeyUsageOptions{}
+	extendedKeyUsage := privatecapb.KeyUsage_ExtendedKeyUsageOptions{}
+	additionalExtensions := []*privatecapb.X509Extension{}
+	if timestamping {
+		baseKeyUsage.ContentCommitment = true
+		extendedKeyUsage.TimeStamping = true
+		timestampExt, err := asn1.Marshal([]asn1.ObjectIdentifier{{1, 3, 6, 1, 5, 5, 7, 3, 8}})
+		if err != nil {
+			return nil
+		}
+		additionalExtensions = append(additionalExtensions, &privateca.X509Extension{
+			ObjectId: &privatecapb.ObjectId{ObjectIdPath: []int32{2, 5, 29, 37}},
+			Critical: true,
+			Value:    timestampExt,
+		})
+	} else {
+		baseKeyUsage.DigitalSignature = true
+		extendedKeyUsage.CodeSigning = true
+	}
+	keyUsage := privatecapb.KeyUsage{
+		BaseKeyUsage:     &baseKeyUsage,
+		ExtendedKeyUsage: &extendedKeyUsage,
+	}
+
+	// TODO, use the right fields :)
+	return &privatecapb.CreateCertificateRequest{
+		Parent: parent,
+		Certificate: &privatecapb.Certificate{
+			// should be 6 months
+			Lifetime: &durationpb.Duration{Seconds: int64(15780000)},
+			CertificateConfig: &privatecapb.Certificate_Config{
+				Config: &privatecapb.CertificateConfig{
+					PublicKey: &privatecapb.PublicKey{
+						Type: privatecapb.PublicKey_PEM_EC_KEY,
+						Key:  pemBytes,
+					},
+					ReusableConfig: &privatecapb.ReusableConfigWrapper{
+						ConfigValues: &privatecapb.ReusableConfigWrapper_ReusableConfigValues{
+							ReusableConfigValues: &privatecapb.ReusableConfigValues{
+								KeyUsage:             &keyUsage,
+								AdditionalExtensions: additionalExtensions,
+							},
+						},
+					},
+					SubjectConfig: &privatecapb.CertificateConfig_SubjectConfig{
+						SubjectAltName: &privatecapb.SubjectAltNames{
+							Uris: []string{uri},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func init() {
+	generateCmd.PersistentFlags().String("gcp-private-ca-parent", "projects/project-rekor/locations/us-central1/certificateAuthorities/sigstore", "private ca parent: /projects/<project>/locations/<location>/<name>")
+	generateCmd.PersistentFlags().StringVar(&pubKeyFile, "public-key-file", "", "path to the PEM encoded public key")
+	generateCmd.PersistentFlags().StringVar(&uri, "uri", "", "uri for the cert")
+	generateCmd.PersistentFlags().String("ct-log-url", "", "host and path (with log prefix at the end) to the ct log")
+	generateCmd.PersistentFlags().Bool("timestamping", false, "Configure certificate for timestamping")
+
+	if err := viper.BindPFlags(generateCmd.PersistentFlags()); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+	rootCmd.AddCommand(generateCmd)
+}
--- go.mod
+++ go.mod
@@ -19,6 +19,7 @@ require (
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/mitchellh/mapstructure v1.4.1
 	github.com/pelletier/go-toml v1.8.1 // indirect
+	github.com/pkg/errors v0.9.1
 	github.com/prometheus/client_golang v1.10.0
 	github.com/rs/cors v1.7.0
 	github.com/segmentio/ksuid v1.0.3
