ea2549814f3f14cf58a3a5166411fc337c008956
Jake Sanders <jsand@google.com>
2021-12-03 14:50:47 -0800

    add the ability to set the user-agent string on requests from the fulcio client
    Signed-off-by: Jake Sanders <jsand@google.com>

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -15,6 +15,7 @@
 package client
 
 import (
+	"net/http"
 	"net/url"
 
 	"github.com/go-openapi/runtime"
@@ -27,9 +28,62 @@ import (
 // SigstorePublicServerURL is the URL of Sigstore's public Fulcio service.
 const SigstorePublicServerURL = "https://fulcio.sigstore.dev"
 
+// Option is a functional option for customizing static signatures.
+type Option func(*options)
+
+type options struct {
+	UserAgent string
+}
+
+func makeOptions(opts ...Option) *options {
+	o := &options{
+		UserAgent: "",
+	}
+
+	for _, opt := range opts {
+		opt(o)
+	}
+
+	return o
+}
+
+// WithUserAgent sets the media type of the signature.
+func WithUserAgent(userAgent string) Option {
+	return func(o *options) {
+		o.UserAgent = userAgent
+	}
+}
+
+type roundTripper struct {
+	http.RoundTripper
+	UserAgent string
+}
+
+// RoundTrip implements `http.RoundTripper`
+func (rt *roundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	req.Header.Set("User-Agent", rt.UserAgent)
+	return rt.RoundTripper.RoundTrip(req)
+}
+
+func createRoundTripper(inner http.RoundTripper, o *options) http.RoundTripper {
+	if inner == nil {
+		inner = http.DefaultTransport
+	}
+	if o.UserAgent == "" {
+		// There's nothing to do...
+		return inner
+	}
+	return &roundTripper{
+		RoundTripper: inner,
+		UserAgent:    o.UserAgent,
+	}
+}
+
 // New returns a new client to interact with the given fulcio server.
-func New(server *url.URL) *genclient.Fulcio {
+func New(server *url.URL, opts ...Option) *genclient.Fulcio {
+	o := makeOptions(opts...)
 	rt := httptransport.New(server.Host, genclient.DefaultBasePath, []string{server.Scheme})
 	rt.Consumers["application/pem-certificate-chain"] = runtime.TextConsumer()
+	rt.Transport = createRoundTripper(rt.Transport, o)
 	return genclient.New(rt, strfmt.Default)
 }
