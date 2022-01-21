ad6b988b1c4aa4c83a8dff5220c14912ef80554d
Nathan Smith <12156185+nsmith5@users.noreply.github.com>
2022-01-21 05:58:29 -0800

    Set max request size to 4MiB (#338)
    Signed-off-by: Nathan Smith <nathan@nfsmith.ca>

---
+++ pkg/api/max_bytes.go
@@ -0,0 +1,27 @@
+// Copyright 2021 The Sigstore Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package api
+
+import "net/http"
+
+// WithMaxBytes sets the max request size on a handler to n bytes.
+func WithMaxBytes(next http.Handler, n int64) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		limitedReader := http.MaxBytesReader(w, r.Body, n)
+		r.Body = limitedReader
+		next.ServeHTTP(w, r)
+	})
+}
