0cf0977f8cc468bde22af2282f9296692310ec41
Nathan Smith <nathan@nfsmith.ca>
2022-01-27 08:44:18 -0600

    Add logging middleware for CTL client
    Signed-off-by: Nathan Smith <nathan@nfsmith.ca>

---
+++ pkg/ctl/logging.go
@@ -0,0 +1,43 @@
+// Copyright 2021 The Sigstore Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package ctl
+
+import (
+	"github.com/sigstore/fulcio/pkg/ca"
+	"go.uber.org/zap"
+)
+
+type loggingClient struct {
+	next   Client
+	logger *zap.SugaredLogger
+}
+
+func (lc *loggingClient) AddChain(csc *ca.CodeSigningCertificate) (*CertChainResponse, error) {
+	lc.logger.Info("Submitting CTL inclusion for subject", csc.Subject.Value)
+	resp, err := lc.next.AddChain(csc)
+	if err != nil {
+		lc.logger.Error("Failed to submit certificate to CTL with error", err)
+		return nil, err
+	}
+	lc.logger.Info("CTL Submission Signature Received: ", resp.Signature)
+	lc.logger.Info("CTL Submission ID Received: ", resp.ID)
+	return resp, nil
+}
+
+// WithLogging adds logging to a certificate transparenct log client
+func WithLogging(next Client, logger *zap.SugaredLogger) Client {
+	return &loggingClient{next, logger}
+}
