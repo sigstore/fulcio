3786a20f437d1bfc294b356c869e4fae2584fb27
cpanato <ctadeu@gmail.com>
2024-01-04 15:34:38 +0100

    update generated files
    Signed-off-by: cpanato <ctadeu@gmail.com>

--- pkg/generated/protobuf/fulcio.pb.gw.go
+++ pkg/generated/protobuf/fulcio.pb.gw.go
@@ -35,11 +35,7 @@ func request_CA_CreateSigningCertificate_0(ctx context.Context, marshaler runtim
 	var protoReq CreateSigningCertificateRequest
 	var metadata runtime.ServerMetadata
 
-	newReader, berr := utilities.IOReaderFactory(req.Body)
-	if berr != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
-	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -52,11 +48,7 @@ func local_request_CA_CreateSigningCertificate_0(ctx context.Context, marshaler 
 	var protoReq CreateSigningCertificateRequest
 	var metadata runtime.ServerMetadata
 
-	newReader, berr := utilities.IOReaderFactory(req.Body)
-	if berr != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
-	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
--- pkg/generated/protobuf/legacy/fulcio_legacy.pb.gw.go
+++ pkg/generated/protobuf/legacy/fulcio_legacy.pb.gw.go
@@ -36,11 +36,7 @@ func request_CA_CreateSigningCertificate_0(ctx context.Context, marshaler runtim
 	var protoReq CreateSigningCertificateRequest
 	var metadata runtime.ServerMetadata
 
-	newReader, berr := utilities.IOReaderFactory(req.Body)
-	if berr != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
-	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -53,11 +49,7 @@ func local_request_CA_CreateSigningCertificate_0(ctx context.Context, marshaler 
 	var protoReq CreateSigningCertificateRequest
 	var metadata runtime.ServerMetadata
 
-	newReader, berr := utilities.IOReaderFactory(req.Body)
-	if berr != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
-	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
