486397ec306f25a295b5bfc1bdad2007f3fa4ee1
Javan lacerda <javanlacerda@google.com>
2024-05-27 17:54:51 +0000

    drafting generic issuer implementation
    Signed-off-by: Javan lacerda <javanlacerda@google.com>

---
+++ pkg/identity/generic/issuer_test.go
@@ -0,0 +1,81 @@
+// Copyright 2023 The Sigstore Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package generic
+
+import (
+	"context"
+	"encoding/json"
+	"testing"
+
+	"github.com/coreos/go-oidc/v3/oidc"
+	"github.com/sigstore/fulcio/pkg/config"
+	"github.com/sigstore/fulcio/pkg/identity"
+)
+
+func TestIssuer(t *testing.T) {
+	ctx := context.Background()
+	url := "test-issuer-url"
+	issuer := Issuer(url)
+
+	// test the Match function
+	t.Run("match", func(t *testing.T) {
+		if matches := issuer.Match(ctx, url); !matches {
+			t.Fatal("expected url to match but it doesn't")
+		}
+		if matches := issuer.Match(ctx, "some-other-url"); matches {
+			t.Fatal("expected match to fail but it didn't")
+		}
+	})
+
+	t.Run("authenticate", func(t *testing.T) {
+		token := &oidc.IDToken{
+			Issuer:  "https://iss.example.com",
+			Subject: "subject",
+		}
+		claims, err := json.Marshal(map[string]interface{}{
+			"aud":            "sigstore",
+			"iss":            "https://iss.example.com",
+			"sub":            "doesntmatter",
+			"email":          "alice@example.com",
+			"email_verified": true,
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+		withClaims(token, claims)
+
+		ctx := config.With(context.Background(), &config.FulcioConfig{
+			OIDCIssuers: map[string]config.OIDCIssuer{
+				"https://iss.example.com": {
+					IssuerURL: "https://iss.example.com",
+					Type:      config.IssuerTypeEmail,
+					ClientID:  "sigstore",
+				},
+			},
+		})
+
+		identity.Authorize = func(_ context.Context, _ string, _ ...config.InsecureOIDCConfigOption) (*oidc.IDToken, error) {
+			return token, nil
+		}
+		principal, err := issuer.Authenticate(ctx, "token")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if principal.Name(ctx) != "alice@example.com" {
+			t.Fatalf("got unexpected name %s", principal.Name(ctx))
+		}
+	})
+}
---
+++ pkg/identity/generic/principal.go
@@ -0,0 +1,164 @@
+// Copyright 2022 The Sigstore Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package generic
+
+import (
+	"bytes"
+	"context"
+	"crypto/x509"
+	"fmt"
+	"net/url"
+	"os"
+	"strings"
+	"text/template"
+
+	"github.com/coreos/go-oidc/v3/oidc"
+	"github.com/sigstore/fulcio/pkg/certificate"
+	"github.com/sigstore/fulcio/pkg/identity"
+	"gopkg.in/yaml.v2"
+)
+
+// TODO: Delete after merged the config reader and get it directly from the github repo
+type RootYaml struct {
+	Providers map[string]Provider
+}
+
+type Provider struct {
+	Subject    string
+	Extensions certificate.Extensions
+	Uris       []string
+	Defaults   map[string]string
+}
+
+func readYaml() RootYaml {
+	var obj RootYaml
+
+	yamlFile, err := os.ReadFile("../../config/config.yaml")
+	if err != nil {
+		fmt.Printf("yamlFile.Get err #%v ", err)
+	}
+	err = yaml.Unmarshal(yamlFile, &obj)
+	if err != nil {
+		fmt.Printf("Unmarshal: %v", err)
+	}
+
+	return obj
+}
+
+func WorkflowPrincipalFromIDToken(_ context.Context, token *oidc.IDToken) (identity.Principal, error) {
+	var claims map[string]string
+	if err := token.Claims(&claims); err != nil {
+		return nil, err
+	}
+
+	yaml := readYaml()
+
+	// It probably doesn't work
+	provider := yaml.Providers[token.Subject]
+	e := provider.Extensions
+	defaults := provider.Defaults
+	finalExtensions := certificate.Extensions{
+		Issuer:                              ApplyTemplate(e.Issuer, claims, defaults),
+		GithubWorkflowTrigger:               ApplyTemplate(e.GithubWorkflowTrigger, claims, defaults),
+		GithubWorkflowSHA:                   ApplyTemplate(e.GithubWorkflowSHA, claims, defaults),
+		GithubWorkflowName:                  ApplyTemplate(e.GithubWorkflowName, claims, defaults),
+		GithubWorkflowRepository:            ApplyTemplate(e.GithubWorkflowRepository, claims, defaults),
+		GithubWorkflowRef:                   ApplyTemplate(e.GithubWorkflowRef, claims, defaults),
+		BuildSignerURI:                      ApplyTemplate(e.BuildSignerURI, claims, defaults),
+		BuildConfigDigest:                   ApplyTemplate(e.BuildConfigDigest, claims, defaults),
+		RunnerEnvironment:                   ApplyTemplate(e.RunnerEnvironment, claims, defaults),
+		SourceRepositoryURI:                 ApplyTemplate(e.SourceRepositoryURI, claims, defaults),
+		SourceRepositoryDigest:              ApplyTemplate(e.SourceRepositoryDigest, claims, defaults),
+		SourceRepositoryRef:                 ApplyTemplate(e.SourceRepositoryRef, claims, defaults),
+		SourceRepositoryIdentifier:          ApplyTemplate(e.SourceRepositoryIdentifier, claims, defaults),
+		SourceRepositoryOwnerURI:            ApplyTemplate(e.SourceRepositoryOwnerURI, claims, defaults),
+		SourceRepositoryOwnerIdentifier:     ApplyTemplate(e.SourceRepositoryOwnerIdentifier, claims, defaults),
+		BuildConfigURI:                      ApplyTemplate(e.BuildConfigURI, claims, defaults),
+		BuildSignerDigest:                   ApplyTemplate(e.BuildSignerDigest, claims, defaults),
+		BuildTrigger:                        ApplyTemplate(e.BuildTrigger, claims, defaults),
+		RunInvocationURI:                    ApplyTemplate(e.RunInvocationURI, claims, defaults),
+		SourceRepositoryVisibilityAtSigning: ApplyTemplate(e.SourceRepositoryVisibilityAtSigning, claims, defaults),
+	}
+	finalUris := make([]string, len(provider.Uris)-1)
+	for _, val := range provider.Uris {
+		finalUris = append(finalUris, ApplyTemplate(val, claims, defaults))
+	}
+
+	return &Provider{
+		Subject:    token.Subject,
+		Extensions: finalExtensions,
+		Uris:       finalUris,
+	}, nil
+}
+
+func ApplyTemplate(path string, data map[string]string, defaultData map[string]string) string {
+
+	// Here we merge the data from was claimed by the id token with the
+	// default data provided by the yaml file.
+	// The order here matter because we want to override the default data
+	// with the claimed data.
+	mergedData := make(map[string]string)
+	for k, v := range defaultData {
+		mergedData[k] = v
+	}
+	for k, v := range data {
+		mergedData[k] = v
+	}
+
+	// It checks it is a path or a raw field by
+	// checking exists template syntax into the string
+	if strings.Contains(path, "{{.") {
+		var doc bytes.Buffer
+		t := template.New("")
+		p, err := t.Parse(path)
+		if err != nil {
+			panic(err)
+		}
+		err = p.Execute(&doc, mergedData)
+		if err != nil {
+			panic(err)
+		}
+		return doc.String()
+	} else {
+		return mergedData[path]
+	}
+}
+
+func (p Provider) Name(_ context.Context) string {
+	return p.Subject
+}
+
+func (p Provider) Embed(_ context.Context, cert *x509.Certificate) error {
+
+	uris := make([]*url.URL, len(p.Uris))
+	for _, value := range p.Uris {
+		url, err := url.Parse(value)
+		if err != nil {
+			panic(err)
+		}
+		uris = append(uris, url)
+	}
+	// Set workflow ref URL to SubjectAlternativeName on certificate
+	cert.URIs = uris
+
+	var err error
+	// Embed additional information into custom extensions
+	cert.ExtraExtensions, err = p.Extensions.Render()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
